6088bafbc4ec0a3026fa477286d0f20647149da5
ytk43200 <zm18146@josai.ac.jp>
2020-10-20 02:10:42 +0000

    message

--- ex06-5.js
+++ ex06-5.js
@@ -2,3 +2,6 @@ var who='world';
 function sayhello() {alert('Hello,'+who+'!');}
 function taro() {who='太郎'}
 function hanako() {who ='花子';}
+
+function taro() {alert('太郎'+'が来た'+'!');}
+function hanako() {alert('花子'+'が来た'+'!');}
