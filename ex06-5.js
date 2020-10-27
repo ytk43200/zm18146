82f8ce3927c691d0f68e9bbe2e55c8f1281c0a2a
ytk43200 <zm18146@josai.ac.jp>
2020-10-27 02:06:10 +0000

    1027

--- ex06-5.js
+++ ex06-5.js
@@ -1,6 +1,6 @@
 var who='world';
-function sayhello65() {alert('Hello,'+who65+'!');}
-function taro65() {who='太郎'}
+function sayhello65() {alert('Hello,'+ who65 +'!');}
+function taro65() {who ='太郎'}
 function hanako65() {who ='花子';}
 function taro65() {alert('太郎'+'が来た'+'!');}
 function hanako65() {alert('花子'+'が来た'+'!');}
