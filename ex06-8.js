ced390dbd1e8cc1ccec4095d645a8f72c48f850b
ytk43200 <zm18146@josai.ac.jp>
2020-11-10 02:00:07 +0000

    1110

--- ex06-8.js
+++ ex06-8.js
@@ -1,3 +1,3 @@
-function sayhello() {alert('Hello,' + document.getElementById('who').innerHTML+ '!');}
-function taro() {document.getElementById('who').innerHTML= '太郎';}
-function hanako() {document.getElementById('who').innerHTML= '花子';}
+function sayhello() {alert('Hello,' + document.getElementById('who68').innerHTML+ '!');}
+function taro68() {document.getElementById('who68').innerHTML= '太郎';}
+function hanako68() {document.getElementById('who68').innerHTML= '花子';}
